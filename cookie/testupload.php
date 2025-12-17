1b495bc23671daa538617366f4b40b10e3f4311c
duyenphamnb29-crypto <duyenphamnb29@gmail.com>
2025-12-17 21:02:17 +0700

    testupload.php

---
+++ cookie/testupload.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <title>Upload File</title>
+</head>
+<body>
+
+    <h1>Upload file to server</h1>
+
+    <form method="post" action="upload.php" enctype="multipart/form-data">
+        <input type="file" name="fileupload" id="fileupload">
+        <input type="submit" name="upload" id="upload" value="Upload">
+    </form>
+
+</body>
+</html>
