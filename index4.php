0061a24317828eb5aa4ac68fd9b66c03c83c31ec
duyenphamnb29-crypto <duyenphamnb29@gmail.com>
2025-12-17 21:46:20 +0700

    Create index4.php

---
+++ index4.php
@@ -0,0 +1,27 @@
+<?php
+	$x = 4;
+	if ($x === 1)
+		echo "Tháng $x có 31 ngày <br>";
+	elseif ($x ===2)
+		echo "Tháng $x có 28 hoặc 29 ngày <br>";
+	elseif ($x ===3)
+		echo "Tháng $x có 31 ngày <br>";
+	elseif ($x ===4)
+		echo "Tháng $x có 30 ngày <br>";
+		
+	//Chuyển sang cấu trúc switch..case
+	$m = 5;
+	switch ($m){
+		case 1:case 3: case 5: case 7: case 8: case 10: case 12:
+			echo "Tháng $m có 31 ngày <br>";
+			break;
+		case 2:
+			echo "Tháng $m có 28 hoặc 29 ngày <<br>";
+			break;
+		case 4: case 6: case 9: case 11:
+			echo "Tháng $m có 30 ngày <br>";
+			break;
+		default:
+			echo "Tháng $m không hợp lệ <br>";
+	}
+?>
