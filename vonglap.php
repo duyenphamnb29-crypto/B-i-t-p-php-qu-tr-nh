70cac3348ac3846f8f3ee259e43711190c6fc1dc
duyenphamnb29-crypto <duyenphamnb29@gmail.com>
2025-12-17 21:17:06 +0700

    vonglap.php

---
+++ vonglap.php
@@ -0,0 +1,10 @@
+<?php
+$cars = ["Toyota", "Volvo", "Mitsubishi", "Honda"];
+foreach ($cars as $value) {
+    echo $value . "<br>";
+}
+for ($i = 0; $i < count($cars); $i++) {
+    echo $cars[$i] . "<br>";
+}
+?>
+
